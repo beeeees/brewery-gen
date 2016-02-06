5b16fb99f3a8e5228ed6fb36943bba3b566a0af8
Bethany Brown <bethany@digital-telepathy.com>
2016-02-05 16:36:13 -0800

    a list of names

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var request = require('request');
+// var BreweryDb = require("brewerydb-node");
+var app = express();
+
+var API_KEY = "836b4b48754b311175827bad9266f44c";
+var url = "http://api.brewerydb.com/v2/locations?key=" + API_KEY + "&locality=san+diego";
+
+app.get('/api', function(req, res){
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var brewresponse = JSON.parse(body);
+      var brewresponse = brewresponse.data;
+      request(url, function (error, response, body) {
+        for (i in brewresponse) {
+          var names = brewresponse[i].brewery.name;
+          console.log(names)
+        }
+      })
+    }
+  })
+});
+
+
+
+// // Get for brewery page, and lists all beers if avialable
+// app.get("/brewery/:id", function(req, res){
+//   var user = req.getUser();
+//   var brewId = req.params.id;
+//   request("http://api.brewerydb.com/v2/breweries?key=" + process.env.brew_db + "&ids=" + brewId, function(err, response, body){
+//     var breweryIndiv = JSON.parse(body);
+//     request("http://api.brewerydb.com/v2/brewery/" + brewId + "/beers?key=" + process.env.brew_db, function(err, response, body){
+//       var beerList = JSON.parse(body);
+//       res.render("brewery", {breweryIndiv:breweryIndiv, user:user, beerList:beerList});
+//     });
+//   });
+// });
+
+
+
+app.listen(3000);
+console.log('Server running on port %d', 3000);
+
+
+
+// // Inital request to get all the breweries from brewerydb and render them to the home page
+// app.get("/", function(req, res){
+//   request("http://api.brewerydb.com/v2/locations?key=" + API_KEY + "&locality=sandiego", function(err, response, body){
+//     res.render("index", {mapData: body, user: user});
+//   });
+// });
+
+
+
+// app.get('/api', function(req, res){
+//   request('http://api.brewerydb.com/v2/locations?key=' + API_KEY + "&locality=san+diego", function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+//       res.send(response);
+//     }
+//    });
+// });
